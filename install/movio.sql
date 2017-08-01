1e3403f3173ed4d3f5340f053fbf502d1ea9af66
Daniele Ugoletti <daniele.ugoletti@gruppometa.it>
2017-08-01 08:12:06 +0200

    Modify mysql encoding from utf8mb4 to utf8

--- install/movio.sql
+++ install/movio.sql
@@ -14,13 +14,7 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `mibac_movio_vuoto`
---
-
--- --------------------------------------------------------
+/*!40101 SET NAMES utf8 */;
 
 --
 -- Table structure for table `countries_tbl`
